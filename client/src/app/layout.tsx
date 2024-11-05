ea73cf03cc903ec4a76b8b4c9cc4ec4eab422ac1
Spartan <venkatsaikedarinath@gmail.com>
2024-11-05 09:25:12 -0800

    addedauth

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,26 +2,16 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import SessionProvider from "@/providers/SessionProvider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
 export const metadata: Metadata = {
-  title: "CircleChat",
-  description: "Chat with friends or anonymous or anyone in the world securely",
+  title: "QuickChat",
+  description: "Quick Chat App",
 };
 
 export default function RootLayout({
@@ -31,14 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
+      <SessionProvider>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </SessionProvider>
     </html>
   );
 }
