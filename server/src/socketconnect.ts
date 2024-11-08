1d44d1cf44c821251a284addc619c411caa856d6
Spartan <venkatsaikedarinath@gmail.com>
2024-11-07 19:40:07 -0800

    addedsocket

--- server/src/socketconnect.ts
+++ server/src/socketconnect.ts
@@ -2,10 +2,13 @@ import { Server } from "socket.io";
 
 export default function setupSocket(io : Server){
     io.on("connection",(socket)=>{
-        console.log("server connected", socket.id)
-        
+        console.log("server connected", socket.id);
+        socket.on("message", (data) =>{
+            console.log("server side message", data)
+            socket.emit("message", data)
+        });
         socket.on("disconnect", () => {
             console.log(" user disconnected", socket.id)
-        })
+        });
     })
 }
