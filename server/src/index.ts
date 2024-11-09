6800e33f4294addf6602a5458bc8acedd227f8ab
Spartan <venkatsaikedarinath@gmail.com>
2024-11-08 16:50:33 -0800

    addedioroom

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,18 +5,24 @@ import Routes from "./routes/index.js"
 import { Server } from "socket.io";
 import { createServer } from "http";
 import setupSocket from "./socketconnect.js";
-
-
-
+import { createAdapter } from "@socket.io/redis-streams-adapter";
+import redis from "./config/redis.js";
+import { instrument } from "@socket.io/admin-ui";
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "*", 
+    origin: ["http://localhost:3000","https://admin.socket.io"], 
+    credentials : true
   },
+  adapter  : createAdapter(redis)
 })
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
 export {io}
 setupSocket(io)
 app.use(cors());
