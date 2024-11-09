9541a2872f38bd3b689e4a4739a78d850509b9f7
Spartan <venkatsaikedarinath@gmail.com>
2024-11-08 16:52:03 -0800

    addedmiddlewareinsocketconnect

--- server/src/socketconnect.ts
+++ server/src/socketconnect.ts
@@ -1,12 +1,25 @@
-import { Server } from "socket.io";
-
+import { Server, Socket } from "socket.io";
+interface CustomSocket extends Socket {
+    room?: string;
+  }
 export default function setupSocket(io : Server){
+   
+    io.use((socket: CustomSocket, next) => {
+        const room = socket.handshake.auth.room || socket.handshake.headers.room;
+        if (!room) {
+          return next(new Error("Invalid room"));
+        }
+        socket.room = room;
+        next();
+      });
     io.on("connection",(socket)=>{
         console.log("server connected", socket.id);
+
         socket.on("message", (data) =>{
             console.log("server side message", data)
-            socket.emit("message", data)
+            socket.broadcast.emit("message", data)
         });
+
         socket.on("disconnect", () => {
             console.log(" user disconnected", socket.id)
         });
