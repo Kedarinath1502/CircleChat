86e556896dcaee7cba4709ea7f63544341a559e3
Spartan <venkatsaikedarinath@gmail.com>
2024-11-17 01:27:27 -0800

    changedid

--- client/src/app/chat/[id]/page.tsx
+++ client/src/app/chat/[id]/page.tsx
@@ -1,20 +1,23 @@
-import ChatBase from '@/components/chat/ChatBase'
+import ChatBase from '@/components/chat/ChatBase';
 import { fetchChats } from '@/fetch/fetchChats';
 import { fetchChatGroup, fetchChatGroupUsers } from '@/fetch/groupFetch';
 import { notFound } from 'next/navigation';
-import React from 'react'
+import React from 'react';
 
 export default async function chat({ params }: { params: { id: string } }) {
-  const { id } = await params
+  const { id } = params; // No `await` here, `params` is already a plain object
+
+  // Validate `id`
   if (id.length !== 36) {
     return notFound();
   }
+
+  // Fetch data
   const chatGroup: GroupChatType | null = await fetchChatGroup(id);
   if (chatGroup === null) {
     return notFound();
   }
-  const chatGroupUsers: Array<GroupChatUserType> | [] =
-    await fetchChatGroupUsers(id);
+  const chatGroupUsers: Array<GroupChatUserType> | [] = await fetchChatGroupUsers(id);
   const chats: Array<MessageType> | [] = await fetchChats(id);
 
   return (
