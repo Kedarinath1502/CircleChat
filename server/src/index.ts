a96f7bab18fe1e96d31c4ea8b3a0fd94c157b797
Spartan <venkatsaikedarinath@gmail.com>
2024-11-27 10:26:03 -0800

    changedindex

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 7000;
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000","https://admin.socket.io"], 
+    origin: ["http://localhost:3000","https://admin.socket.io","https://circle-chat.vercel.app"], 
     credentials : true
   },
   adapter  : createAdapter(redis)
